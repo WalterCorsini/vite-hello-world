5b85ebe34e8626f86361e69d01a88ad449ddec1b
WalterCorsini <corsini.walter@gmail.com>
2024-04-30 14:11:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!-- script VueJs -->
+<script>
+import AppTitle from "./components/AppTitle.vue";   // import components url
+export default{
+  components:{  // import components
+    AppTitle,
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<!-- HTML -->
+<template>
+
+<AppTitle />  <!-- use components in template-->
+
+</template>
+
+<!-- CSS -->
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite",
+      image: "sfondo-vite.jpg",
+    }
+  },
+  methods:{ 
+    createUrlImage(imageName) {
+        return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+    <img :src="createUrlImage(image)" />
+</template>
+
+<style>
+    img{
+        width: 70%;
+    }
+</style>
